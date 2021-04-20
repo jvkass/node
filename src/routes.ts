a894450d4010fc6ed619f9f9467e3a9729fb789a
jvkass <jv.mata@hotmail.com>
2021-04-20 16:31:02 -0300

    Banco de Dados 100% funcional mais controller de Settings

--- src/routes.ts
+++ src/routes.ts
@@ -1,34 +1,10 @@
 import {Router} from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "./repositories/SettingsRepository";
+import { SettingsController } from "./controllers/SettingsController";
 
 const routes = Router();
 
-/**
- * Tipos de parametros
- * Routes Params => Parametros de rotas
- * http://localhost:3333/settings/1
- * Query Params => Filtros e buscas
- * http://localhost:3333/settings/1?search=algumacoisa
- * 
- * Body params => {
- * 
- * }
- */
+const settingsController = new SettingsController();
 
-routes.post("/settings", async (request, response) =>{
-  const {chat,username} = request.body;
-
-  const settingsRepository = getCustomRepository(SettingsRepository)
-
-  const settings = settingsRepository.create({
-    chat,
-    username
-  });
-
-  await settingsRepository.save(settings);
-
-  return response.json(settings);
-})
+routes.post("/settings", settingsController.create);
 
 export {routes};
